5934d171e427d01774532122fccbf294b1750b6d
zbudairi <160100247+zbudairi@users.noreply.github.com>
2025-04-25 21:20:28 +0700

    first

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const navbar = document.querySelector(".navbar");
+
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+})
