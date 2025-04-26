d752b7d588b0dacb308fe177dd2f1650d19363b7
zbudairi <160100247+zbudairi@users.noreply.github.com>
2025-04-26 13:53:34 +0700

    penambahan hamburger button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-const navbar = document.querySelector(".navbar");
+const menuNav = document.querySelector(".navbar");
+const menuBar = document.querySelector(".hamburger");
+const menuNavigation = document.querySelector(".menu");
 
+menuBar.addEventListener('click', () => {
+    menuBar.classList.toggle("is-active");
+    menuNavigation.classList.toggle('menu-active');
+    
+
+});
 
 window.addEventListener('scroll',()=>{
-    if(window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-})
+    menuNav.classList.toggle('scrolled', window.scrollY > 0);
+
+    windowPosition = window.scrollY > 200;
+    menuBar.classList.toggle("btn-active", windowPosition);
+   
+});
